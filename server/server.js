62869b100221773ec3159b5a7be3bb977afccccf
Brian Oh <ohbrian@Brians-MacBook-Pro.local>
2019-06-21 10:34:12 -0400

    code cleanup

--- server/server.js
+++ server/server.js
@@ -9,8 +9,8 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(cors());
 // app.use(morgan('dev'));
+app.use(cors());
 app.use(express.static(path.join(__dirname, '../client/dist/')));
 
 app.get('/:number', (req, res) => {
